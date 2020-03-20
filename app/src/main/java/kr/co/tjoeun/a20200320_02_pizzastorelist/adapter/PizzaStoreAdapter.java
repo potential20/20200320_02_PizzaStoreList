6e5664d535ca0898dc3f1214d47d603c742c411c
potential20 <passion212@nate.com>
2020-03-20 12:04:18 +0900

    피자 가게 어댑터 뼈대 작업.

---
+++ app/src/main/java/kr/co/tjoeun/a20200320_02_pizzastorelist/adapter/PizzaStoreAdapter.java
@@ -0,0 +1,44 @@
+package kr.co.tjoeun.a20200320_02_pizzastorelist.adapter;
+
+// 어댑터 클래스의 뼈대 작업.
+// 사용자 / 사용변수 3개 / getView 오버라이딩
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+import java.util.zip.Inflater;
+
+import kr.co.tjoeun.a20200320_02_pizzastorelist.R;
+import kr.co.tjoeun.a20200320_02_pizzastorelist.datas.PizzaStore;
+
+public class PizzaStoreAdapter extends ArrayAdapter<PizzaStore> {
+
+    Context mContext;
+    List<PizzaStore> mList;
+    LayoutInflater inf;
+
+    public PizzaStoreAdapter(@NonNull Context context, int resource, @NonNull List<PizzaStore> objects) {
+        super(context, resource, objects);
+
+        mContext = context;
+        mList = objects;
+        inf = LayoutInflater.from(mContext); // 직접 작성
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+        if (row == null) {
+            row = inf.inflate(R.layout.pizza_store_list_item,null);
+        }
+        return row;
+    }
+}
