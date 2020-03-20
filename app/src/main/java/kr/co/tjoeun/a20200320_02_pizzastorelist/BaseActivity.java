23c06b4e1780acbf08d9740a0e8b4ba9208250c2
potential20 <passion212@nate.com>
2020-03-20 11:43:25 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/a20200320_02_pizzastorelist/BaseActivity.java
@@ -0,0 +1,13 @@
+package kr.co.tjoeun.a20200320_02_pizzastorelist;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+}
