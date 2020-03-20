5cdf9a97448b8dbb6226411735a8cc5f67020aae
potential20 <passion212@nate.com>
2020-03-20 15:45:05 +0900

    전화 걸기 권한 획득 - TedPermission 라이브러리 활용

--- app/src/main/java/kr/co/tjoeun/a20200320_02_pizzastorelist/PizzaStoreDetailActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200320_02_pizzastorelist/PizzaStoreDetailActivity.java
@@ -3,12 +3,18 @@ package kr.co.tjoeun.a20200320_02_pizzastorelist;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 import kr.co.tjoeun.a20200320_02_pizzastorelist.databinding.ActivityPizzaStoreDetailBinding;
 import kr.co.tjoeun.a20200320_02_pizzastorelist.datas.PizzaStore;
@@ -33,12 +39,39 @@ public class PizzaStoreDetailActivity extends BaseActivity {
             @Override
             public void onClick(View v) {
 //                가게의 전화번호를 => CALL 액션으로 연결.
+//                  사용자의 허가가 OK인 상황에서 실행하도록.
+//                  => TedPermission 라이브러리 활용
 
-                String phoneNumUri = String.format("tel:%s",store.getPhoneNum());
+                PermissionListener pl = new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+//                        허가가 떨어진 상황 => 전화걸기 코드 실행
+                        String phoneNumUri = String.format("tel:%s",store.getPhoneNum());
 
-                Uri uri = Uri.parse(phoneNumUri);
-                Intent intent = new Intent(Intent.ACTION_CALL, uri);
-                startActivity(intent);
+                        Uri uri = Uri.parse(phoneNumUri);
+                        Intent intent = new Intent(Intent.ACTION_CALL, uri);
+                        startActivity(intent);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(List<String> deniedPermissions) {
+//                        권한을 사용자가 거부한 상황 =>
+//                        전화 권한을 허가 해야 사용 가능합니다. Toast
+                        Toast.makeText(mContext, "전화 권한을 허가 해야 사용 가능합니다.", Toast.LENGTH_SHORT).show();
+
+                    }
+                };
+
+//                onClick 실행? 사용자가 손으로 클릭 할때 실행
+//                권한 상황 대응 실행? 코드로 권한 요청을 직접 날려서 실행.
+
+                TedPermission.with(mContext)
+                        .setPermissionListener(pl)
+                        .setDeniedMessage("[설정]에서 허용해줘야 사용가능합니다.")
+                        .setPermissions(Manifest.permission.CALL_PHONE)
+                        .check();
+
+
 
             }
         });
