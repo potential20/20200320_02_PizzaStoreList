1df3325381eb596b9c3d32a62d6d076bf0b74072
potential20 <passion212@nate.com>
2020-03-20 14:41:27 +0900

    피자 가게 상세화면 추가

---
+++ app/src/main/java/kr/co/tjoeun/a20200320_02_pizzastorelist/PizzaStoreDetailActivity.java
@@ -0,0 +1,31 @@
+package kr.co.tjoeun.a20200320_02_pizzastorelist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import kr.co.tjoeun.a20200320_02_pizzastorelist.databinding.ActivityPizzaStoreDetailBinding;
+
+public class PizzaStoreDetailActivity extends BaseActivity {
+
+    ActivityPizzaStoreDetailBinding binding = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_pizza_store_detail);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
