2d5fec071c461fd98b524956217a945b65910b18
potential20 <passion212@nate.com>
2020-03-20 15:14:10 +0900

    피자가게 누르면 상세화면에서 정보 반영

--- app/src/main/java/kr/co/tjoeun/a20200320_02_pizzastorelist/PizzaStoreDetailActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200320_02_pizzastorelist/PizzaStoreDetailActivity.java
@@ -5,7 +5,10 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
+
 import kr.co.tjoeun.a20200320_02_pizzastorelist.databinding.ActivityPizzaStoreDetailBinding;
+import kr.co.tjoeun.a20200320_02_pizzastorelist.datas.PizzaStore;
 
 public class PizzaStoreDetailActivity extends BaseActivity {
 
@@ -26,6 +29,12 @@ public class PizzaStoreDetailActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+
+        PizzaStore store = (PizzaStore) getIntent().getSerializableExtra("store");
+
+        binding.storeNameTxt.setText(store.getStoreName());
+        binding.storePhoneTxt.setText(store.getPhoneNum());
+        Glide.with(mContext).load(store.getLogoUrl()).into(binding.storeLogoImg);
 
     }
 }
